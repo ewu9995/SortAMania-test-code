c26c95aa14d80664628d4cdf6ce4b73c069d7806
Author <PC@Dell>
2018-03-01 19:08:17 -0500

    challenges 1-3 finished

--- Sortamania/src/Runner.java
+++ Sortamania/src/Runner.java
@@ -27,10 +27,49 @@ public class Runner {
 		return arr;
 	}
 	
+	public static int[] mostlySortedInts (int x){
+		Team9SortCompetition q = new Team9SortCompetition();
+		int Sortedindexes =0;
+		int Unsortedindexes =0;
+		int arrindex =0; 
+		
+		int[] Sortedarr = new int [(int) (x*.75)];
+		int [] Unsortedarr = new int [x - Sortedarr.length];
+		int [] arr = new int [x];
+		
+		for(int i=0; i <Sortedarr.length-1; i++) {
+			Sortedarr[i] = (int) (Math.random()*x);
+		}
+		
+		q.quickSort(Sortedarr, 0, Sortedarr.length-1);
+		
+		for(int a=0; a <Unsortedarr.length-1; a++) {
+			Unsortedarr[a] = (int) (Math.random()*x);
+		}
+		
+		
+		for(int b=0; b <Sortedarr.length-1; b++) {
+			arr[b] = Sortedarr[Sortedindexes];
+			Sortedindexes ++;
+			arrindex ++;
+		}
+		
+		for(int c= Sortedarr.length; c <arr.length-1; c++) {
+			arr[c] = Unsortedarr[Unsortedindexes];
+			Unsortedindexes++;
+			arrindex++;
+		}
+		
+		
+		return arr;
+	}
+	
+	
 	public static void main(String[] args) {
 		
 		int [] randArr = randomInts(10000);
 		String [] randStrings = randomStrings(10000);
+		int [] randArr2 = mostlySortedInts(10000);
 		
 		Team9SortCompetition x = new Team9SortCompetition();
 		
@@ -41,7 +80,7 @@ public class Runner {
 		System.out.println("Challenge one took: " + time + " nanoseconds");
 		System.out.println(Arrays.toString(randArr));
 		System.out.println(x.challengeOne(randArr));
-		System.out.println(randArr[4999]);
+	
 		
 		start = System.nanoTime();
 		end = System.nanoTime();
@@ -49,7 +88,15 @@ public class Runner {
 		x.challengeTwo(randStrings, "abcde");
 		System.out.println("Challenge two took: " + time + " nanoseconds");
 		System.out.println(x.challengeTwo(randStrings, "abcde"));
-	
+		
+		start = System.nanoTime();
+		end = System.nanoTime();
+		time = end - start;
+		x.challengeOne(randArr2);
+		System.out.println("Challenge three took: " + time + " nanoseconds");
+		System.out.println(Arrays.toString(randArr2));
+		System.out.println(x.challengeThree(randArr2));
+
 	
 	}
 	
--- Sortamania/src/Team9SortCompetition.java
+++ Sortamania/src/Team9SortCompetition.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class Team9SortCompetition extends SortCompetition{
 
@@ -13,7 +14,7 @@ public class Team9SortCompetition extends SortCompetition{
 		String[] arr1 = arr;
 		String x = query;
 		int y = 0;
-		bubbleSort(arr1);
+		mergeSort(arr1);
 		for(int i =0; i < arr1.length; i++) {
 			if(arr1[i].equals(x)) {
 				y= i;
@@ -25,8 +26,9 @@ public class Team9SortCompetition extends SortCompetition{
 
 	@Override
 	public int challengeThree(int[] arr) {
-		// TODO Auto-generated method stub
-		return 0;
+		int[] arr1 = arr;
+		quickSort(arr1, 0, arr1.length-1);
+		return arr1[(arr1.length/2)-1];
 	}
 
 	@Override
@@ -53,7 +55,7 @@ public class Team9SortCompetition extends SortCompetition{
 		arr[index2] = temp;
 	}
 	
-	public static void quickSort(int[] list1, int front, int back)
+	public void quickSort(int[] list1, int front, int back)
 	{
 		if(back>front) {
 			int pivotPos = partition(list1, front, back);
@@ -73,6 +75,68 @@ public class Team9SortCompetition extends SortCompetition{
 					}
 				}
 			}
+		}
+	
+	public static String[] mergeSort(String[] list) 
+	{
+		//Base case
+		if(list.length == 1)
+			return list;
+		//Recursive step
+		
+		//two arrays left and right since merge sort is splitting up arrays by half then combining it back together sorted
+		//left is 0 to to 1/2
+		//right is 1/2 to end
+		String[] left = Arrays.copyOfRange(list, 0, list.length/2);
+		String[] right = Arrays.copyOfRange(list, list.length/2 , list.length); 
+		return(merge(mergeSort(left), mergeSort(right)));
+		
+	}
+	
+	//Sourced from: https://stackoverflow.com/questions/5958169/how-to-merge-two-sorted-arrays-into-a-sorted-array
+
+	public static String[] merge(String[] list1 , String[] list2) {
+
+			String [] combinedArray = new String[list1.length + list2.length]; //new array must be length of both arrays combined
+			int x=0; //list1 indexes
+			int y=0; //list2 indexes
+			int z=0; //new array (combinedArray)
+			boolean a = false;
+			  while (x < list1.length && y < list2.length) //both arrays must be less than their intended length (any longer with break the code)
+		        {
+		            if (list1[x].compareTo(list2[y]) <= 0) //compares the values in the 2 arrays and see which is smaller
+		            {
+		               combinedArray[z] = list1[x]; //since the value in list1 is smaller in this case, list1[x] becomes part of the sorted array before list2[y]
+		                x++; //x will count up as the values are placed into the merged array
+		                z++; //z is the merged array index so it counts up as it fills in its slots
+		            }
+		            else  
+		            {
+		            	combinedArray[z] = list2[y]; //since list2[y] is smaller, it is placed before list1[x]
+		                y++; //y counts up as values are placed into merged array
+		                z++;
+		            }
+		            
+		        	}
+			  		
+			  	
+			  		//loops through the arrays to find values
+			  		while (x < list1.length)
+			  		{
+			  			combinedArray[z] = list1[x];
+			  			x++;
+			  			z++;
+			  		}
+
+			  		while (y < list2.length)
+			  		{
+			  			combinedArray[z] = list2[y];
+			  			y++; 
+			  			z++;
+			  		}
+			  	
+
+			    return combinedArray; //returns the merged sorted array 
 		}
 	
 	public static void bubbleSwap(String []arr, int index1, int index2) {
